5ff7df8b50f83de909f53748b97105ddc630f04f
revandpratama <revandaustin@gmail.com>
2024-03-11 09:20:56 +0700

    fix: minor update

--- main.go
+++ main.go
@@ -3,10 +3,21 @@ package main
 import (
 	"fmt"
 
+	"github.com/gin-gonic/gin"
 	"github.com/revandpratama/go-simple-blog-api/config"
+	"github.com/revandpratama/go-simple-blog-api/routes"
 )
 
 func main() {
 	fmt.Println("Starting config...")
 	config.LoadConfig()
+
+	fmt.Println("Database initiated..")
+	config.LoadDB()
+	
+	r := gin.Default()
+
+	api := r.Group("/api")
+
+	routes.PostRoutes(api)
 }
